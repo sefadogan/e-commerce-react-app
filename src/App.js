53992bf4636ab1e160e969bdaac76421f7f905bc
sefadogan <sefadogan7@gmail.com>
2020-03-27 00:45:45 +0300

    created project and added files for homepage.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
+import HomePage from './homepage.component';
 
-class App extends Component {
-  render() {
-    return <div className='App'>Hello World</div>;
-  }
+function App() {
+  return (
+    <HomePage />
+  );
 }
 
 export default App;
