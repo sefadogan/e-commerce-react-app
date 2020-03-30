2dc024168e896b2d93cbe0f2da2b3642314daee2
sefadogan <sefadogan7@gmail.com>
2020-03-30 21:05:29 +0300

    Finished sign in handleSubmit method.

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils'
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -17,10 +17,16 @@ class SignIn extends React.Component {
 		};
 	}
 
-	handleSubmit = (e) => {
+	handleSubmit = async (e) => {
 		e.preventDefault();
 
-		this.setState({ email: '', password: '' });
+		const { email, password } = this.state;
+		try {
+			await auth.signInWithEmailAndPassword(email, password);
+			this.setState({ email: '', password: '' });
+		} catch (error) {
+			console.log('An unexpected error occured while creating user. Exception message: ' + error.message);
+		}
 	};
 
 	handleChange = (e) => {
@@ -54,7 +60,9 @@ class SignIn extends React.Component {
 					/>
 					<div className="buttons">
 						<CustomButton type="submit">Sign In</CustomButton>
-						<CustomButton onClick={signInWithGoogle} isGoogleSignIn={true}>Sign In With Google</CustomButton>
+						<CustomButton onClick={signInWithGoogle} isGoogleSignIn={true}>
+							Sign In With Google
+						</CustomButton>
 					</div>
 				</form>
 			</div>
